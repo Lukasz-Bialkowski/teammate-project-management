00f677306f87a8a6de00773e0f25b983b0e0a6fc
Lukasz Bialkowski <lukasz.adrian.bialkowski@gmail.com>
2016-10-09 13:03:49 +0200

    Bower components upgrade. TODO Slider is now broken.

--- src/main/resources/static/src/app/home/home.controller.js
+++ src/main/resources/static/src/app/home/home.controller.js
@@ -4,9 +4,7 @@ homeModule.controller( 'HomeCtrl', [ function HomeCtrl( ) {
     var vm = this;
     vm.myInterval = 3000;
     vm.slides = [
-        { image: 'http://placehold.it/1900x1080&text=Slide One', text: 'blah' },
-        { image: 'http://placehold.it/1900x1080&text=Slide Two', text: 'somthing' },
-        { image: 'http://placehold.it/1900x1080&text=Slide Three', text: 'teammate' }
+        {image: 'http://placehold.it/1140x520&text=Slide One', text: 'blah'}
     ];
 
 }]);

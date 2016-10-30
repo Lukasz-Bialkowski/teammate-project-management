95db68dacdce95fa0cb47f1cee0f7851357ba4e2
Lukasz Bialkowski <lukasz.adrian.bialkowski@gmail.com>
2016-10-30 16:42:50 +0100

    Task List hiding and autosave of task status.

--- src/main/resources/static/src/app/home/home.controller.js
+++ src/main/resources/static/src/app/home/home.controller.js
@@ -1,10 +1,22 @@
 var homeModule = angular.module( 'teammateApp.home');
 
-homeModule.controller( 'HomeCtrl', [ function HomeCtrl( ) {
+homeModule.controller('HomeCtrl', ['$scope', function HomeCtrl($scope) {
     var vm = this;
-    vm.myInterval = 3000;
-    vm.slides = [
-        {image: 'http://placehold.it/1140x520&text=Slide One', text: 'blah'}
-    ];
+    $scope.myInterval = 5000;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var currIndex = 0;
 
+    $scope.addSlide = function () {
+        var newWidth = 1140 + slides.length + 1;
+        slides.push({
+            image: '//unsplash.it/' + newWidth + '/520',
+            text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
+            id: currIndex++
+        });
+    };
+
+    for (var i = 0; i < 4; i++) {
+        $scope.addSlide();
+    }
 }]);
